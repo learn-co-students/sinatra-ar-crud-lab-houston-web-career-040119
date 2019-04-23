95d3d75dbe6e8f789b4cd930cb5a8bde2a724e2e
smrbts <samueljustinroberts@gmail.com>
2019-04-23 17:08:11 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,49 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to '/articles'
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get "/articles/new" do
+    @article = Article.new
+    erb :new
+  end
+
+  post "/articles" do
+    @article = Article.create(params)
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  get "/articles/:id" do
+    @article = current_article
+    erb :show
+  end
+
+  get "/articles/:id/edit" do
+    @article = current_article
+    erb :edit
+  end
+
+  patch "/articles/:id" do
+    @article = current_article
+    @article.update(params[:article])
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  delete "/articles/:id" do
+    Article.destroy(params[:id])
+    redirect to "/articles"
+  end
+
+
+def current_article
+  Article.find(params[:id])
+end
+
+
 end
