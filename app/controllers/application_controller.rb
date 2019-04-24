c71936fd57f8eb1225ae03771f6254aff13e61dc
PYN <yxp7531540@gmail.com>
2019-04-23 20:51:00 -0500

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,47 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all
+
+    erb :index
+  end
+
+  get '/articles/new' do
+
+    erb :new
+  end
+
+  post '/articles' do
+    article = Article.create(title: params[:title], content: params[:content])
+    redirect "/articles/#{article.id}"
+  end
+
+  get '/articles/:id' do |id|
+
+    @article = Article.find(id)
+    erb :show
+  end
+
+  get '/articles/:id/edit' do |id|
+    @article = Article.find(id)
+
+    erb :edit
+  end
+
+  patch '/articles/:id' do |id|
+
+    article = Article.find(id)
+    article.update(title: params[:title], content: params[:content])
+
+    redirect "/articles/#{id}"
+  end
+
+  delete '/articles/:id' do |id|
+    Article.find(id).destroy
+  end
+
+
+
 end
